835613b621b978474e54758f84fafef753c117f9
knoori-code <knoori3@gmail.com>
2023-02-11 22:27:15 -0500

    Add orders and status to restaurant page

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -3,7 +3,7 @@ const db = require("../connection");
 const getOrders = () => {
   return db
     .query(
-      "SELECT dishes.name AS dish_name, order_id, quantity, users.name AS customer_name FROM orders JOIN order_details ON orders.id = order_id JOIN users ON user_id = users.id JOIN dishes ON dish_id = dishes.id;"
+      "SELECT dishes.name AS dish_name, order_id, quantity, users.name AS customer_name, orders.status AS status FROM orders JOIN order_details ON orders.id = order_id JOIN users ON user_id = users.id JOIN dishes ON dish_id = dishes.id;"
     )
     .then((data) => {
       return data.rows;
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,20 +1,50 @@
-const express = require('express');
-const router  = express.Router();
-const { getOrders } = require('../db/queries/orders');
-const { getUserById } = require('../db/queries/users');
+const express = require("express");
+const router = express.Router();
+const { getOrders } = require("../db/queries/orders");
+const { getUserById } = require("../db/queries/users");
 
-
-router.get('/restaurant-order', (req, res) => {
+router.get("/restaurant-order", (req, res) => {
   getUserById(req.session.user_id).then((user) => {
-    if (user.role === 'res') {
+    if (user.role === "res") {
       getOrders().then((orders) => {
+        const cleanOrders = {};
+        for (const detail of orders) {
+          if (cleanOrders[detail.order_id]) {
+            cleanOrders[detail.order_id]["dishes"].push([
+              detail["dish_name"],
+              detail["quantity"],
+            ]);
+          } else {
+            cleanOrders[detail.order_id] = {
+              order_id: detail.order_id,
+              customer_name: detail.customer_name,
+              dishes: [[detail["dish_name"], detail["quantity"]]],
+              status: detail["status"],
+            };
+          }
+        }
+        console.log(cleanOrders);
         const templateVars = {
-          orders
+          cleanOrders,
         };
+
+        // const newObj = {
+        //   1: {
+        //     order_id: 1,
+        //     customer_name: "Viet",
+        //     dishes: [["pasta",3], ["steak", 5]]
+        //   },
+        //   2: {
+        //     order_id: 2,
+        //     customer_name: "Alex",
+        //     dishes: [["pasta",3], ["steak", 5]]
+        //   }
+        // }
+
         return res.render("restaurant_page", templateVars);
       });
     } else {
-      return res.redirect('/');
+      return res.redirect("/");
     }
   });
 });
