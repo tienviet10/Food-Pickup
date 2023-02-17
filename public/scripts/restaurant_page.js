9d4cfe40ed29ef4f9f35c2fc03ec0d3b423e2fc7
Viet Tran <viettran101294@gmail.com>
2023-02-16 19:07:07 -0500

    add placeholder for phone field on registration page

--- public/scripts/restaurant_page.js
+++ public/scripts/restaurant_page.js
@@ -147,52 +147,6 @@ $(() => {
       });
     };
 
-    // const formTemplate = `
-    // <div id="prep_${order.order_id}">
-    //   <form class="form">
-    //     <div class="form-group">
-    //       <label for="duration">Duration:</label>
-    //       <input data-id="${order.order_id}" class="form-control" type="text" name="duration" style="width: 300px" />
-    //     </div>
-    //     <button type="submit" class="btn btn-success">
-    //       Prep
-    //     </button>
-    //   </form>
-    // </div>
-    // `;
-
-    // let displayDish = '';
-
-    // for (let dish of order.dishes) {
-    //   displayDish += (`<div>Dish: ${dish[0]}; Quantity:${dish[1]}</div>`);
-    // }
-
-    // const $order = $(`
-    //   <div>
-    //     <div>${order.order_id} ${order.customer_name} ${order.status}</div>
-    //     ${displayDish}
-    //     ${order.status === 'pending' ? formTemplate : ''}
-    //   </div>
-    // `);
-
-    // const $form = $order.find('form');
-    // $form.on("submit", function(event) {
-    //   event.preventDefault();
-    //   const inputField = $(this).find('input[name="duration"]');
-    //   const orderId = inputField.data("id");
-    //   const duration = inputField.val();
-    //   const expectedCompletion = ($.now() + duration * 60000) / 1000;
-    //   const removeForm = "prep_" + orderId;
-
-    //   $.post(
-    //     "/api/restaurants/accept-order",
-    //     { orderId, expectedCompletion },
-    //     function(data, status) {
-    //       $(`#${removeForm}`).empty();
-    //       //$currentOrder.remove();
-    //     }
-    //   );
-    // });
     return $order;
   };
 
